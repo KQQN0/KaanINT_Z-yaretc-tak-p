a47bd69c4baf840b29044057e82e9c50e1672aac
KQQN0 <kqanyimz@gmail.com>
2025-05-26 00:23:26 +0300

    app.py

--- app.py
+++ app.py
@@ -286,3 +286,7 @@ def export_json():
 if __name__ == '__main__':
     init_db() # Veritabanını başlat ve tabloları oluştur
     app.run(debug=True)
+
+#import os 
+#if __name__ == "__main__":
+#    app.run(host="0.0.0" , port=int(os.environ.get("PORT", 500)))
